b07a0878ee41037b5616f68db6f6edf7aa13a6b7
Pavel_T <tpsside@gmail.com>
2019-10-13 21:45:49 +0300

    implement InMemoryUserRepository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,43 +3,81 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
+    private Map<Integer, User> repository = new ConcurrentHashMap<Integer, User>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        this.save( new User(counter.incrementAndGet(),
+                "Admin",
+                "admin@mail.ru",
+                "123",
+                MealsUtil.DEFAULT_CALORIES_PER_DAY,
+                true,
+                Collections.singleton(Role.ROLE_ADMIN)));
+
+        this.save( new User(counter.incrementAndGet(),
+                "FirstUser",
+                "first@mail.ru",
+                "456",
+                MealsUtil.DEFAULT_CALORIES_PER_DAY,
+                true,
+                Collections.singleton(Role.ROLE_USER)));
+    }
+
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return repository.remove(id) != null;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+            repository.put(user.getId(), user);
+            return user;
+        }
+        // update without saving
+        return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return repository.get(id);
     }
 
     @Override
-    public List<User> getAll() {
+    public Collection<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return repository.values();
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
+        for (Map.Entry<Integer, User> entry : repository.entrySet()) {
+            if (entry.getValue().getEmail() == email) {
+                return entry.getValue();
+            }
+        }
         return null;
     }
 }
