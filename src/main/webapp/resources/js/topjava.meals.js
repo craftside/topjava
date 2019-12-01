7c855fad22fb7e8f3bde20914fcb25e8a75a7702
Pavel_T <tpsside@gmail.com>
2019-12-01 18:41:57 +0300

    add create/delete methods in MealAjaxController

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,42 @@
+$(function () {
+    makeEditable({
+            ajaxUrl: "ajax/profile/meals/",
+            datatableApi: $("#datatable").DataTable({
+                "paging": false,
+                "info": true,
+                "columns": [
+                    {
+                        "data": "dateTime"
+                    },
+                    {
+                        "data": "description"
+                    },
+                    {
+                        "data": "calories"
+                    },
+                    {
+                        "defaultContent": "Edit",
+                        "orderable": false
+                    },
+                    {
+                        "defaultContent": "Delete",
+                        "orderable": false
+                    }
+                ],
+                "order": [
+                    [
+                        0,
+                        "desc"
+                    ]
+                ]
+            })
+        }
+    );
+
+    $('#dateTime').datetimepicker(
+        {
+            format: 'Y-m-d\\TH:i',
+            // lang: 'ru'
+        }
+    );
+});
