e17548aa5362091c211d5039148980c26e009fb0
Pavel_T <tpsside@gmail.com>
2019-12-03 22:05:27 +0300

    add nullable to MealAjaxController#getBetween

--- src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.web.meal;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.to.MealTo;
@@ -42,10 +43,10 @@ public class MealAjaxController extends AbstractMealController {
 
     @Override
     @GetMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<MealTo> getBetween(@RequestParam LocalDate startDate,
-                                   @RequestParam LocalTime startTime,
-                                   @RequestParam LocalDate endDate,
-                                   @RequestParam LocalTime endTime) {
+    public List<MealTo> getBetween(@RequestParam  @Nullable LocalDate startDate,
+                                   @RequestParam  @Nullable LocalTime startTime,
+                                   @RequestParam  @Nullable LocalDate endDate,
+                                   @RequestParam  @Nullable LocalTime endTime) {
         return super.getBetween(startDate, startTime, endDate, endTime);
     }
 }
